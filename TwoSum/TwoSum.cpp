4964ecdae5edae88742b84e27cc18b6a737a5caa
EmersiX <54605544+EmersiX@users.noreply.github.com>
2020-10-05 02:22:03 +0200

    Changed twoSum function to Two-pass Hash Table.

--- TwoSum/TwoSum.cpp
+++ TwoSum/TwoSum.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <map>
+#include <algorithm>
 
-std::vector<int> testVector = {1,2,3};
+std::vector<int> testVector = { 1,2,3 };
 int testInt = 5;
 
 std::vector<int> twoSum(std::vector<int> &nums, int target)
 {
-    std::unordered_map<int, int> umap;
+    std::unordered_map<int, int> m1;
+    std::vector<int> p1;
+
+    for (int i = 0; i < nums.size(); i++)
+        m1[nums[i]] = i;
+
     for (int i = 0; i < nums.size(); i++)
     {
-        int complement = target - nums[i];
-        std::unordered_map<int, int>::iterator it = umap.find(complement);
+        if (target != nums[i] * 2 && m1.find(target - nums[i]) != m1.end())
+        {
+            p1.push_back(m1[target - nums[i]]);
+            p1.push_back(i);
+            break;
+        }
 
-        if (it != umap.end())
-            return std::vector<int>{i, it->second};
-
-        umap[nums[i]] = i;
+        else if (target == nums[i] * 2 && m1[nums[i]] != i)
+        {
+            p1.push_back(m1[target - nums[i]]);
+            p1.push_back(i);
+            break;
+        }
     }
+
+    return p1;
 }
 
 int main()
